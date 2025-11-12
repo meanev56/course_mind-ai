2069396d9f37f7614574246d92a5d31adf32aec2
meanev56 <emekanjoku86@gmail.com>
2025-11-12 03:07:12 +0100

    Redesigns landing screen with welcoming UI
    Introduces a visually engaging landing page featuring a header image,
    improved color scheme, and welcoming text. Enhances initial user
    impression and provides clearer messaging about the platform’s purpose.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,54 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import Colors from "../constant/Colors";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        backgroundColor: Colors.WHITE,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image 
+        source={require('./../assets/images/landing.png')}
+        style={{
+          width: '100%',
+          height: 300,
+        }}
+      />
+      <View
+        style={{
+          padding: 25,
+          backgroundColor: Colors.PRIMARY,
+          height: '100%',
+          borderTopLeftRadius: 35,
+          borderTopRightRadius: 35,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            textAlign: 'center',
+            color: Colors.WHITE,
+            fontWeight: 'outfit-bold',
+          }}
+        >
+          Welcome to Mind AI
+        </Text>
+        <Text
+          style={{
+            fontSize: 20,
+            color: Colors.WHITE,
+            textAlign: 'center',
+            marginTop: 20,
+            fontFamily: 'outfit',
+          }}
+        >
+          Transform your ideas into engaging educational content, effortlessly
+          with our AI-powered platform 📚📺
+        </Text>
+
+      </View>
     </View>
   );
 }
